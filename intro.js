1ff2d8b717b9f6789d1cede5a460e5150e103752
incognito-dev07 <osasanolusola2007@gmail.com>
2026-09-07 18:36:42 +0100

    Initial commit: Javascript intro assignment

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Osasan Olusola"
+let age = 19
+let favLanguage = "TypeScript"
+
+console.log(`My name is ${name}`)
+console.log(`I am ${age} years old`)
+console.log(`My favourite programming language is ${favLanguage}`)
